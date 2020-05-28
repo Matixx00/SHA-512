314ffb01a96f68c0dcb17a8040e5a8a33893576e
Mateusz Suchocki <suchy.mat@onet.pl>
2020-05-28 20:06:27 +0200

    📚 Add useful links to main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+# List of useful links:
+#
+# Secure Hash Standard
+# https://csrc.nist.gov/csrc/media/publications/fips/180/2/archive/2002-08-01/documents/fips180-2.pdf
+#
+# C++ implementation
+# http://www.zedwood.com/article/cpp-sha512-function
+#
+# C implementation
+# https://akkadia.org/drepper/SHA-crypt.txt
+#
+# JavaScript implementation
+# http://www.movable-type.co.uk/scripts/sha512.html
+#
+# Python implementation
+# https://github.com/thomdixon/pysha2/blob/master/sha2/sha512.py
+
+
 if __name__ == '__main__':
     print("Hello world")
